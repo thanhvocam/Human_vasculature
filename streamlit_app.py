4cdb1e5b1adde63de69f99e6072d18dada7a5346
bacnv <thanhvocam94@gmail.com>
2023-10-27 18:17:21 +0700

    add download weights from gg drive

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 import cv2
+import gdown
 
+url = "https://drive.google.com/uc?id=1xZL5HjrVFsp4Zu6osnc_btM1NfiBcoop"
+output = "best.pt"
 
 def main():
     st.title("Human Vasculature Image Segmentation")  
