aa423f20bf3b288e3212ab5f72e1e807be670909
thanhvocam <thanhvocam94@gmail.com>
2023-11-01 17:14:47 +0700

    add uploading image feature for the streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,57 +13,40 @@ if not os.path.exists(model_path):
 
 def main():
     st.title("Human Vasculature Image Segmentation")
-    col1, col2 = st.columns(2)
-    with col1:
-        # Choose img to test
-        val_path = "val"
-        image_files = os.listdir(val_path)
-        selected_image = st.selectbox("Choose Image to see the Result before upload your Image", image_files)
-        image_path = os.path.join(val_path, selected_image)
-        image_val = Image.open(image_path)
-        st.image(image_val, caption="Image_Test", use_column_width=True)
-        
-        start1 = time.time()
-        # Load YOLO model
-        model = YOLO(model_path)
+    
+    # Choose img to test
+    val_path = "val"
+    image_files = os.listdir(val_path)
+    st.header("Select an image to see the prediction or upload an image below")
+    selected_image = st.selectbox("", image_files)
+    image_path = os.path.join(val_path, selected_image)
+    image = Image.open(image_path)
+    st.image(image, caption="Test image", use_column_width=True)
+    predict(model_path=model_path, image=image)
 
-        # Perform object detection
-        history = model.predict(image_val)[0]
-        image_val_pred = history.plot()
-        
-        # Save and Display the image
-        save_path = 'data_cus/result/image_pred.png'
-        cv2.imwrite(save_path, image_val_pred)
-        end1 = time.time()
-        process_pred1 = end1 - start1
-        with st.spinner("Running"):
-            time.sleep(process_pred1)
-            st.image(image_val_pred, caption="Segmentation Objects", use_column_width=True)
-            st.success("Image saved successfully.")
-        
-        
-    with col2:
-        uploaded_file = st.file_uploader("Upload an image to get prediction", type=["jpg", "jpeg", "png", "tif"])
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
-            st.image(image, caption='Image before predict', use_column_width=True)      
+    st.header("Upload an image to get prediction")
+    uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png", "tif"])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Image before predict', use_column_width=True)      
+        predict(model_path=model_path, image=image)
 
-            start2 = time.time()                
-            # Load YOLO model
-            model = YOLO(model_path)            
-            # Perform object detection
-            history = model.predict(image)[0]
-            image_after_pred = history.plot()
-            
-            # Save and Display the image
-            save_path = 'data_cus/result/image_pred.png'
-            cv2.imwrite(save_path, image_after_pred)
-            end2 = time.time()
-            process_pred2 = end2 - start2
-            with st.spinner("Running"):
-                time.sleep(process_pred2)
-                st.image(image_after_pred, caption="Segmentation Objects", use_column_width=True)
-                st.success("Image saved successfully.")
+def predict(model_path, image):
+    start = time.time()                
+    # Load YOLO model
+    model = YOLO(model_path)            
+    # Perform object detection
+    history = model.predict(image)[0]
+    image_after_pred = history.plot()
+    
+    # Save and Display the image
+    save_path = 'data_cus/result/image_pred.png'
+    cv2.imwrite(save_path, image_after_pred)
+    end = time.time()
+    pred_time = end - start
+    with st.spinner("Running"):
+        time.sleep(pred_time)
+        st.image(image_after_pred, caption="Prediction result", use_column_width=True)
 
 if __name__ == "__main__":
     main()
